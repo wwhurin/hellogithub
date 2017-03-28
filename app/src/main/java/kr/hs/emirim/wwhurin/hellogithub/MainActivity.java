8cb05bb0024ac81d205efa245715ffd172a6f870
wwhurin <wwhurin@e-mirim.hs.kr>
2017-03-28 20:55:02 +0900

    #2 버튼 누르기

--- app/src/main/java/kr/hs/emirim/wwhurin/hellogithub/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhurin/hellogithub/MainActivity.java
@@ -1,13 +1,23 @@
 package kr.hs.emirim.wwhurin.hellogithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHello= (Button) findViewById(R.id.ButtonHello);
+        btnHello.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        Toast.makeText( this, "눌렀다", Toast.LENGTH_SHORT).show();
     }
 }
